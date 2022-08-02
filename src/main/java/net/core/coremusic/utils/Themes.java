9693517c22fb96478a89f7c313b921a5107a0ea2
amiran <azimi6370@gmail.com>
2022-08-02 15:06:11 +0430

    update: themes order

--- src/main/java/net/core/coremusic/utils/Themes.java
+++ src/main/java/net/core/coremusic/utils/Themes.java
@@ -5,8 +5,8 @@ import java.util.Objects;
 public enum Themes {
     LIGHT("light", Objects.requireNonNull(Themes.class.getResource("/net/core/coremusic/themes/light-theme.css")).toExternalForm()),
     DARK("dark", Objects.requireNonNull(Themes.class.getResource("/net/core/coremusic/themes/dark-theme.css")).toExternalForm()),
-    MANJARO_DARK("manjaro-dark", Objects.requireNonNull(Themes.class.getResource("/net/core/coremusic/themes/manjaro-dark-theme.css")).toExternalForm()),
-    MANJARO_LIGHT("manjaro-light", Objects.requireNonNull(Themes.class.getResource("/net/core/coremusic/themes/manjaro-light-theme.css")).toExternalForm());
+    MANJARO_LIGHT("manjaro-light", Objects.requireNonNull(Themes.class.getResource("/net/core/coremusic/themes/manjaro-light-theme.css")).toExternalForm()),
+    MANJARO_DARK("manjaro-dark", Objects.requireNonNull(Themes.class.getResource("/net/core/coremusic/themes/manjaro-dark-theme.css")).toExternalForm());
 
     private final String name, path;
 
